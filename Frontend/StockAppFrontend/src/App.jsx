e042643345d940e199021b8e5e983eb6ce3c94f6
shishirm18 <shishirbittu1997@gmail.com>
2025-03-27 03:10:45 -0700

    React for frontend

---
+++ Frontend/StockAppFrontend/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Stock App coming up: visualize and analyze your favourite stocks and get their insights, fundamentals and latest news updates</h1>
+    </>
+  )
+}
+
+export default App
